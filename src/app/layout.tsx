0005d4fd42fb4b327796fd4d7581363335ee0c96
Ammar Iqbal <ammariqbal043@gmail.com>
2024-07-26 08:55:21 +0500

    add another page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 export const runtime = 'edge'
 
@@ -18,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Link href='/'>Home</Link>
+        <Link href='/page'>Page</Link>
+        {children}
+      </body>
     </html>
   )
 }
